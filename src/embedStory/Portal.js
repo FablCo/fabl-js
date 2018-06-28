ebacee6b79590dd4c77f492f9ceccc8d452c0cfd
ashchedrin <ashchedrin@klika-tech.com>
2018-06-28 16:46:23 +0300

    Add not sensetive offset parameter for frame

--- src/embedStory/Portal.js
+++ src/embedStory/Portal.js
@@ -5,8 +5,9 @@ export default class Portal {
 
   constructor(props) {
     this.props = props;
-    this.listenScroll();
-    this.listenMessage();
+    window.addEventListener('scroll', this.scroll);
+    window.addEventListener('message', this.message);
+    window.addEventListener('resize', this.resize);
   }
 
   generateFrame = () => {
@@ -19,6 +20,7 @@ export default class Portal {
     this.frame.addEventListener("load", () => {
       this.postMessage({ name: "ping" });
       this.postMessage({ name: "subscribe", subscriberName: "frame", subscriberGroup: "scroll" });
+      this.callIfExists(this.props.didLoad);
     });
 
     return this.frame;
@@ -51,51 +53,59 @@ export default class Portal {
     this.getScrollTop() >= this.frameContentHeight - window.innerHeight + this.frameOffsetTop
   )
 
-  listenScroll = () => {
-    window.addEventListener('scroll', () => {
-      const frameScrollTop = this.getFrameScrollTop();
+  scroll = () => {
+    const frameScrollTop = this.getFrameScrollTop();
 
-      if (this.frameIsViewed()) {
-        this.frame.setAttribute('style', Portal.defaultFrameStyles + "position: fixed; top: 0; width:" + this.props.embedTo.offsetWidth + "px");
+    if (this.frameIsViewed()) {
+      this.frame.setAttribute('style', Portal.defaultFrameStyles + "position: fixed; top: 0; width:" + this.props.embedTo.offsetWidth + "px");
+      this.postMessage({ name: 'scroll', scrollPos: frameScrollTop });
+    } else {
+      if (this.frameHasBeenViewed()) {
+        this.frame.setAttribute('style', Portal.defaultFrameStyles + "position: absolute; bottom: 0; top: auto;");
         this.postMessage({ name: 'scroll', scrollPos: frameScrollTop });
       } else {
-        if (this.frameHasBeenViewed()) {
-          this.frame.setAttribute('style', Portal.defaultFrameStyles + "position: absolute; bottom: 0; top: auto;");
-        } else {
-          this.frame.setAttribute('style', Portal.defaultFrameStyles + "position: relative;");
-          this.postMessage({ name: 'scroll', scrollPos: frameScrollTop });
-        }
+        this.frame.setAttribute('style', Portal.defaultFrameStyles + "position: relative;");
+        this.postMessage({ name: 'scroll', scrollPos: frameScrollTop });
       }
-    });
+    }
   }
 
-  listenMessage = () => {
-    window.addEventListener("message", (event) => {
-      try {
-        const data = JSON.parse(event.data);
-        if (data.name === 'ping' && data.response === 'pong') {
-          this.postMessage({ name: 'prepareForIframe' });
-          this.postMessage({ name: 'totalHeight' });
-        }
-        if (data.name === 'totalHeight') {
-          this.frameContentHeight = data.response;
-          this.props.embedTo.setAttribute('style', "height: " + data.response + "px; position: relative");
-        }
-        if (data.name === 'scroll') {
-          window.scrollTo(0, data.scroll + this.frameOffsetTop);
-        }
-      } catch (exception) {
-        console.log('Ignored command', event);
+  message = () => {
+    try {
+      const data = JSON.parse(event.data);
+      if (data.name === 'ping' && data.response === 'pong') {
+        this.postMessage({ name: 'prepareForIframe' });
+        this.postMessage({ name: 'totalHeight' });
       }
-    });
+      if (data.name === 'totalHeight') {
+        this.frameContentHeight = data.response;
+        this.props.embedTo.setAttribute('style', "height: " + data.response + "px; position: relative");
+      }
+      if (data.name === 'scroll') {
+        window.scrollTo(0, data.scroll + this.frameOffsetTop);
+      }
+    } catch (exception) {
+      console.log('Ignored command', event);
+    }
+  }
+
+  resize = () => {
+    this.frame.setAttribute("height", window.innerHeight);
   }
 
   embed = () => {
     this.props.embedTo.appendChild(this.generateFrame());
     this.frameOffsetTop = this.frame.offsetTop;
+    this.frameOffsetTop += this.props.notSenseOffsetTop ? this.props.notSenseOffsetTop : 0;
   }
 
   postMessage = (postObject) => {
     this.frame.contentWindow.postMessage(JSON.stringify(postObject), '*');
+  }
+
+  callIfExists = (func, params = {}) => {
+    if (func) {
+      func(params);
+    }
   }
 }
--- src/embedStory/index.js
+++ src/embedStory/index.js
@@ -2,10 +2,10 @@ import { validateElement, validateString } from "../validators";
 import Portal from "./Portal";
 
 const functionName = "embedStory";
-export default function embedStory(_element, _storyUrl) {
+export default function embedStory(_element, _storyUrl, params = {}) {
   const element = validateElement(_element, `${functionName}: expected first argument to be a DOM element`);
   const storyUrl = validateString(_storyUrl, `${functionName}: expected second argument to be a Fabl story URL`);
 
-  const portal = new Portal({ url: storyUrl, embedTo: element });
+  const portal = new Portal({ url: storyUrl, embedTo: element, ...params });
   portal.embed();
 }
